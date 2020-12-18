d84b0a275a3139c3a5264d57800eeadc3f4cf0b8
agustoni <cibuy731@gmail.com>
2020-12-18 18:39:35 +0700

    banner video

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+//container
+import BannerVideo from "./Container/BannerVideo.js"
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BannerVideo videoUrl="indomie_default.mp4"></BannerVideo>
   );
 }
 
