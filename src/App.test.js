97cf03451c65eda7854c38d69f5926beee84bbfe
adrianusrio <adrianusrio73@gmail.com>
2020-12-18 18:42:25 +0700

    create cardproduct

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,27 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import logo from './logo.svg';
+import './App.css';
+import CardProduct from './Containers/CardProduct';
+import {Container, Row} from 'reactstrap';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+function App() {
+  return (
+    <div className="App">
+      <Container>
+        <Row>
+          <CardProduct grid={'col-md-3 col-lg-3 col-sm-3 col-xs-3 my-4'} image={'logo-indomie-soto.png'} title={'Indomie Soto'} bodytext={'Test'} ></CardProduct>
+          <CardProduct grid={'col-md-3 col-lg-3 col-sm-3 col-xs-3 my-4'} image={'logo-indomie-soto.png'} title={'Indomie Soto'} bodytext={'Test'} ></CardProduct>
+          <CardProduct grid={'col-md-3 col-lg-3 col-sm-3 col-xs-3 my-4'} image={'logo-indomie-soto.png'} title={'Indomie Soto'} bodytext={'Test'} ></CardProduct>
+          <CardProduct grid={'col-md-3 col-lg-3 col-sm-3 col-xs-3 my-4'} image={'logo-indomie-soto.png'} title={'Indomie Soto'} bodytext={'Test'} ></CardProduct>
+          <CardProduct grid={'col-md-3 col-lg-3 col-sm-3 col-xs-3 my-4'} image={'logo-indomie-soto.png'} title={'Indomie Soto'} bodytext={'Test'} ></CardProduct>
+          <CardProduct grid={'col-md-3 col-lg-3 col-sm-3 col-xs-3 my-4'} image={'logo-indomie-soto.png'} title={'Indomie Soto'} bodytext={'Test'} ></CardProduct>
+          <CardProduct grid={'col-md-3 col-lg-3 col-sm-3 col-xs-3 my-4'} image={'logo-indomie-soto.png'} title={'Indomie Soto'} bodytext={'Test'} ></CardProduct>
+          <CardProduct grid={'col-md-3 col-lg-3 col-sm-3 col-xs-3 my-4'} image={'logo-indomie-soto.png'} title={'Indomie Soto'} bodytext={'Test'} ></CardProduct>
+          <CardProduct grid={'col-md-3 col-lg-3 col-sm-3 col-xs-3 my-4'} image={'logo-indomie-soto.png'} title={'Indomie Soto'} bodytext={'Test'} ></CardProduct>
+          <CardProduct grid={'col-md-3 col-lg-3 col-sm-3 col-xs-3 my-4'} image={'logo-indomie-soto.png'} title={'Indomie Soto'} bodytext={'Test'} ></CardProduct>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
